46aae3393a476221c43f5249d88d911c8a3c98a6
salsabilalilanti <salsabilalilanti091175@gmail.com>
2025-02-11 14:09:51 +0700

    Add files via upload

---
+++ assets/slide.js
@@ -0,0 +1,22 @@
+var a=-100;
+setInterval(function(){
+    if (a!=0) {
+        slide = document.getElementById('slide');
+        slide.style="margin-left:"+a+"%;transition: 1s ease-in;";
+        if (a== -300) {
+            a=0;
+        }else{
+            a=a+-100;
+        }
+        return a;
+    }else{
+        slide = document.getElementById('slide');
+        slide.style="margin-left:"+a+"%;";
+        if (a== -300) {
+            a=0;
+        }else{
+            a=a+-100;
+        }
+        return a;
+    }
+}, 2000);
